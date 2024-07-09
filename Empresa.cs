df6fdaa51a32fdc39ca921cad82637b5f3c4b617
Gustavo Machado Lupim <90060817+oLupim@users.noreply.github.com>
2024-07-08 21:58:34 -0300

    changes 1

--- Empresa.cs
+++ Empresa.cs
@@ -23,9 +23,20 @@ public class Empresa
 
     public void ExibirFuncionarios()
     {
-        foreach (var Funcionario in ListaFuncionarios)
+        for (int i = 0; i < ListaFuncionarios.Count; i++)
         {
-            Funcionario.ExibirInformacoes();
+            ListaFuncionarios[i].ExibirInformacoes();
+
+            if (ListaFuncionarios[i].Projetos.Count > 0)
+            {
+                Console.WriteLine($"Projetos: {string.Join(", ", ListaFuncionarios[i].Projetos)}");
+            }
+
+            else
+            {
+                Console.WriteLine("Projetos: Nenhum Projeto");
+            }
+            Console.WriteLine();
         }
     }
 }
